96b5ce45637dfdce887e2c5860acc534c0cff6a7
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-07-27 21:33:22 -0500

    Update CustomLogoutHandler.java

---
+++ BackEnd/src/main/java/com/DIYTracker/BackEnd/config/CustomLogoutHandler.java
@@ -0,0 +1,30 @@
+
+
+@Configuration
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    public CustomLogoutHandler(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        Token storedToken = tokenRepository.findByToken(token).orElse(null);
+
+        if(storedToken != null) {
+            storedToken.setLoggedOut(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}
